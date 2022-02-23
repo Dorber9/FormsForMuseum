04175105018a4d18990fff1dfa6bf73c897bf481
Yuval <yuvalmaron1924@gmail.com>
2022-02-23 19:29:53 +0200

    yuval's commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,16 +6,18 @@ import {
   MenuItem,
   Container,
 } from "react-bootstrap";
+import { FaHome, FaArchway, FaSearchDollar } from "react-icons/fa";
 
 const Header = () => {
   return (
     <>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Museum</Navbar.Brand>
+          <Navbar.Brand href="/">Museum <FaArchway /> </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Data" id="basic-nav-dropdown">
+            <Nav.Link href="/">Home <FaHome /></Nav.Link>
+            
+            <NavDropdown title="Data" id="basic-nav-dropdown"  > 
               <NavDropdown.Item href="/Add">Add</NavDropdown.Item>
               <NavDropdown.Item href="/ModifyData">Modify</NavDropdown.Item>
 
@@ -24,7 +26,7 @@ const Header = () => {
                 Course Wizard
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#pricing">Pricing <FaSearchDollar /></Nav.Link>
           </Nav>
         </Container>
       </Navbar>

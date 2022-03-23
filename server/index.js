2ae5d508cae5c487fdda0e1ddc684a4049abea49
EitanIvcha <93527501+EitanIvcha@users.noreply.github.com>
2022-03-23 19:53:06 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -411,6 +411,7 @@ app.post("/addItem", (req, res) => {
     );
 });
 app.put("/updateItem", (req, res) => {
+    console.log("HI";
     const ID = req.body.ID;
     const name = req.body.name;
     const descr = req.body.descr;

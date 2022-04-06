607fc21379a9ec0dd82e5c070e5852340fc0c8b8
YuvalMar <87989270+YuvalMar@users.noreply.github.com>
2022-04-06 20:10:19 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -637,7 +637,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1000000 },
+  limits: { fileSize: 10000000000 },
 }).single("myImage");
 
 // test
